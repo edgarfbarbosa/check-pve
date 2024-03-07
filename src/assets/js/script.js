c64c643d6fbcdc9f09c5118c59b446010b2fdaf7
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2024-03-07 09:18:24 -0300

    feat: adiciona tratamento de erro e interface CharacterData

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,8 +1,16 @@
 "use strict";
 async function getCharacterInfo(character) {
-    const CHARACTER_PROFILE_URL = `https://raider.io/api/v1/characters/profile?region=${character.region}&realm=${character.realm}&name=${character.name}&fields=gear%2Cmythic_plus_scores_by_season%3Acurrent`;
-    const response = await fetch(CHARACTER_PROFILE_URL);
-    const data = await response.json();
-    console.log(data);
+    try {
+        const CHARACTER_PROFILE_URL = `https://raider.io/api/v1/characters/profile?region=${character.region}&realm=${character.realm}&name=${character.name}&fields=gear%2Cmythic_plus_scores_by_season%3Acurrent`;
+        const response = await fetch(CHARACTER_PROFILE_URL);
+        const data = await response.json();
+        setCharacterInfo(data);
+    }
+    catch (error) {
+        console.error(error);
+    }
 }
-getCharacterInfo({ region: "us", realm: "Azralon", name: "Edrawr" });
+getCharacterInfo({ region: "us", realm: "Azralon", name: "Eddh" });
+function setCharacterInfo(data) {
+    console.log(data.mythic_plus_scores_by_season[0]?.scores?.all);
+}
--- src/assets/js/script.ts
+++ src/assets/js/script.ts
@@ -5,11 +5,36 @@ interface CharacterInfo {
 }
 
 async function getCharacterInfo(character: CharacterInfo) {
-  const CHARACTER_PROFILE_URL = `https://raider.io/api/v1/characters/profile?region=${character.region}&realm=${character.realm}&name=${character.name}&fields=gear%2Cmythic_plus_scores_by_season%3Acurrent`
-  const response = await fetch(CHARACTER_PROFILE_URL)
-  const data = await response.json()
-  
-  console.log(data)
+  try {
+    const CHARACTER_PROFILE_URL = `https://raider.io/api/v1/characters/profile?region=${character.region}&realm=${character.realm}&name=${character.name}&fields=gear%2Cmythic_plus_scores_by_season%3Acurrent`
+    const response = await fetch(CHARACTER_PROFILE_URL)
+    const data = await response.json()
+    
+    setCharacterInfo(data)
+  }
+  catch(error) {
+    console.error(error)
+  }
 }
 
-getCharacterInfo({region: "us", realm: "Azralon", name: "Edrawr"})
+getCharacterInfo({region: "us", realm: "Azralon", name: "Eddh"})
+
+interface CharacterData {
+  name: string;
+  race: string;
+  class: string;
+  active_spec_name: string;
+  faction: string;
+  thumbnail_url: string;
+  region: string;
+  realm: string;
+  mythic_plus_scores_by_season: Array<{
+    scores: {
+      all: number
+    }
+  }>
+}
+
+function setCharacterInfo(data: CharacterData) {
+  console.log(data.mythic_plus_scores_by_season[0]?.scores?.all)
+}
