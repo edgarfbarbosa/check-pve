e0791d3384dc30d470c8f6c8df850e28a9d15b7e
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2024-03-06 21:36:30 -0300

    refactor: move URL da requisição para uma constante dentro de getCharacterInfo

--- src/assets/js/script.ts
+++ src/assets/js/script.ts
@@ -5,10 +5,11 @@ interface CharacterInfo {
 }
 
 async function getCharacterInfo(character: CharacterInfo) {
-  const response = await fetch(`https://raider.io/api/v1/characters/profile?region=${character.region}&realm=${character.realm}&name=${character.name}&fields=gear%2Cmythic_plus_scores_by_season%3Acurrent`)
+  const CHARACTER_PROFILE_URL = `https://raider.io/api/v1/characters/profile?region=${character.region}&realm=${character.realm}&name=${character.name}&fields=gear%2Cmythic_plus_scores_by_season%3Acurrent`
+  const response = await fetch(CHARACTER_PROFILE_URL)
   const data = await response.json()
   
   console.log(data)
 }
 
-getCharacterInfo({region: "us", realm: "Azralon", name: "Edbtw"})
+getCharacterInfo({region: "us", realm: "Azralon", name: "Edrawr"})
