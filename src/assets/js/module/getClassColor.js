1caf40245be00a7db9cfe3d7d220cdc6ab940508
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2024-03-20 12:11:27 -0300

    refactor: renomeia e atualiza função getRaceColor para getClassColor

---
+++ src/assets/js/module/getClassColor.js
@@ -0,0 +1,32 @@
+export default function getClassColor(characterClass) {
+    switch (characterClass) {
+        case 'Death Knight':
+            return 'text-death-knight';
+        case 'Demon Hunter':
+            return 'text-demon-hunter';
+        case 'Druid':
+            return 'text-druid';
+        case 'Evoker':
+            return 'text-evoker';
+        case 'Hunter':
+            return 'text-hunter';
+        case 'Mage':
+            return 'text-mage';
+        case 'Monk':
+            return 'text-monk';
+        case 'Paladin':
+            return 'text-paladin';
+        case 'Priest':
+            return 'text-priest';
+        case 'Rogue':
+            return 'text-rogue';
+        case 'Shaman':
+            return 'text-shaman';
+        case 'Warlock':
+            return 'text-warlock';
+        case 'Warrior':
+            return 'text-warrior';
+        default:
+            return 'text-yellow';
+    }
+}
