796f04e971047d4d11b9f93126e8e8172b46be56
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2024-03-20 12:09:27 -0300

    feat(tailwind): adiciona cores personalizadas para todas as classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,21 @@ module.exports = {
       },
       colors: {
         'yellow': '#F8B700',
+        'alliance': '#6699FF',
         'horde': '#E02929',
-        'alliance': '#6699FF'
+        'death-knight': '#C41F3B',
+        'demon-hunter': '#A330C9',
+        'druid': '#FF7D0A',
+        'evoker': '#33937F',
+        'hunter': '#ABD473',
+        'mage': '#69CCF0',
+        'monk': '#00FF96',
+        'paladin': '#F58CBA',
+        'priest': '#FFFFFF',
+        'rogue': '#FFF569',
+        'shaman': '#0070DE',
+        'warlock': '#9482C9',
+        'warrior': '#C79C6E'
       }
     },
   },
