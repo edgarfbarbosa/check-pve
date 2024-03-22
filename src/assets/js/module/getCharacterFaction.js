8e22ab1d32c7e86a0335da7cde7b4ca49d3fa6a4
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2024-03-22 19:15:21 -0300

    feat(getCharacterFaction): adiciona função para manipular classes específicas baseadas na facção do personagem

---
+++ src/assets/js/module/getCharacterFaction.js
@@ -0,0 +1,12 @@
+export default function getCharacterFaction(name, faction, characterBanner) {
+    name.classList.remove('text-alliance', 'text-horde');
+    characterBanner.classList.remove('bg-alliance-banner', 'bg-horde-banner', 'bg-200');
+    if (faction == 'alliance') {
+        name.classList.add('text-alliance');
+        characterBanner.classList.add('bg-alliance-banner', 'bg-200');
+    }
+    else {
+        name.classList.add('text-horde');
+        characterBanner.classList.add('bg-horde-banner', 'bg-200');
+    }
+}
