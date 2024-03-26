40cab82f5014a28b8414d9b2ed994e67ad3c3415
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2024-03-26 14:24:57 -0300

    feat(getCharacterInfo): adiciona chamada de API em paralelo utilizando Promise.all()

--- src/assets/js/module/getCharacterInfo.js
+++ src/assets/js/module/getCharacterInfo.js
@@ -3,19 +3,25 @@ import handleBadRequest from './handleBadRequest.js';
 export default async function getCharacterInfo(character) {
     try {
         const CHARACTER_PROFILE_URL = `https://raider.io/api/v1/characters/profile?region=${character.region}&realm=${character.realm}&name=${character.name}&fields=gear%2Cmythic_plus_scores_by_season%3Acurrent`;
-        const response = await fetch(CHARACTER_PROFILE_URL, {
-            method: 'GET'
-        });
-        switch (response.status) {
+        const MYTHIC_PLUS_AFFIXES_URL = `https://raider.io/api/v1/mythic-plus/affixes?region=${character.region}&locale=pt`;
+        const response = await Promise.all([
+            fetch(CHARACTER_PROFILE_URL, {
+                method: 'GET'
+            }),
+            fetch(MYTHIC_PLUS_AFFIXES_URL, {
+                method: 'GET'
+            })
+        ]);
+        switch (response[0].status) {
             case 200:
-                const data = await response.json();
+                const data = await response[0].json();
                 setCharacterInfo(data);
                 break;
             case 400:
                 handleBadRequest();
                 break;
             default:
-                alert(`HTTP response status codes: ${response.status} (${response.ok})`);
+                alert(`HTTP response status codes: ${response[0].status} (${response[0].ok})`);
         }
     }
     catch (error) {
