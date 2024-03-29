e6182e8dd76e14ef59f1c1208d4c606adb8fe9bd
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2024-03-29 16:14:45 -0300

    feat(handleNotFound): adiciona função handleNotFound para tratar erros 404

---
+++ src/assets/js/module/handleNotFound.js
@@ -0,0 +1,6 @@
+export default function handleNotFound() {
+    const affixesSection = document.getElementById('affixesSection');
+    affixesSection?.classList.remove('block');
+    affixesSection?.classList.add('hidden');
+    alert("Erro 404: Sua requisição não foi encontrada. Verifique se a URL da API está em funcionamento.");
+}
